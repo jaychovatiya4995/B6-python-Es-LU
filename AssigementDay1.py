755fdf4dc0551fbca4c97c92bbe43c5c13110b7e
jaychovatiya4995 <67356213+jaychovatiya4995@users.noreply.github.com>
2020-07-20 16:47:39 +0530

    Create AssigementDay1.py

---
+++ AssigementDay1.py
@@ -0,0 +1,9 @@
+# 1. What is python ? What are the ways to install the python in your pc ?
+# ANS. Python : Python is a popular programming language. It was created by Guido van Rossum, and released
+#              in 1991.
+              
+#     Installion :
+#                  Use the link https://www.python.org/downloads/ to download python installer.
+
+# 2.What are Code Enviorment ?
+# ANS. A code environment is a standalone and self-contained environment to run Python code or R code.
